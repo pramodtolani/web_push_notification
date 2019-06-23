bc68aec97e21df2236e562f25541ccd97008737f
Pramod Tolani <pramodtolani91@gmail.com>
2019-06-23 20:13:19 +0530

    First commit

---
+++ firebase/init.js
@@ -0,0 +1,10 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyAwmFtg4Ls8x7-gPQGXEseL_RaTEfvcMDU",
+  "databaseURL": "https://test-b5efa.firebaseio.com",
+  "storageBucket": "test-b5efa.appspot.com",
+  "authDomain": "test-b5efa.firebaseapp.com",
+  "messagingSenderId": "746683729893",
+  "projectId": "test-b5efa",
+  "appId": "1:746683729893:web:0b6c5b68936e00d0"
+});
